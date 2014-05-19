da27d656c6ea0f9d8531ffb25f6aeb2f54574eff
Ajema <geabw@yahoo.de>
2014-05-19 14:13:21 +0200

    Programm Counter hinzugefügt.

--- src/register/MemoryManagementUnit.java
+++ src/register/MemoryManagementUnit.java
@@ -157,6 +157,25 @@ public class MemoryManagementUnit
 
     }
 
+    //Gibt den PC zurück
+    public int getPC() throws InvalidRegisterException
+    {
+        return (getRegister("PCLATH").getIntValue() << 8) + getRegister("PCL").getIntValue();
+    }
 
+    //Setzt den PC auf übergebenen Wert
+    public void setPC(int pc) throws InvalidRegisterException {
+        //Oberen 5 bit im PCLATH speichern
+        getRegister("PCLATH").setIntValue ((pc & 0x1F00) >> 0b1000); //0b0001 1111 0000 0000 >> 0b0000 0000 0001 1111
+        //Unteren 8 bit im PCL speichern
+        getRegister("PCL").setIntValue(pc & 0x00FF);    //0b0000 0000 1111 1111
+    }
+
+
+    //Inkrementiert den PC
+    public void incPC() throws InvalidRegisterException
+    {
+        setPC(getPC() + 1);
+    }
 
 }
